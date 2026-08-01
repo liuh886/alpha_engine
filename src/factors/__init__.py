02c794ba6a870219f5f6a7981f730db29203f510
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-01 20:27:58 +0800

    research: expose governed factor catalog primitives

---
+++ src/factors/__init__.py
@@ -0,0 +1,6 @@
+"""Governed, versioned factor-library primitives."""
+
+from src.factors.catalog import FactorCatalog
+from src.factors.definition import FactorDefinition
+
+__all__ = ["FactorCatalog", "FactorDefinition"]
