e6d88dc8ef3ba65932e3d8685abb3d3803a42906
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-02 11:27:12 +0800

    frontend: remove legacy home runtime branch

--- qlib-dashboard/src/pages/ArtifactStudioHomePage.tsx
+++ qlib-dashboard/src/pages/ArtifactStudioHomePage.tsx
@@ -1,9 +1,7 @@
 import { useOutletContext } from 'react-router-dom';
 import { ArtifactHome } from '@/components/ArtifactHome';
-import { runtimeCapabilities } from '@/lib/runtime-capabilities';
 import { useGlobalStore } from '@/store/globalStore';
 import type { ModelData } from '@/lib/data-parser';
-import { HomePage } from './HomePage';
 
 interface AppContext {
   models: ModelData[];
@@ -12,10 +10,6 @@ interface AppContext {
 export function ArtifactStudioHomePage() {
   const { models } = useOutletContext<AppContext>();
   const generatedAt = useGlobalStore((state) => state.dataGeneratedAt);
-
-  if (runtimeCapabilities.backendApi) {
-    return <HomePage />;
-  }
 
   return (
     <ArtifactHome
